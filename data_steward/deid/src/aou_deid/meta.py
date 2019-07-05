48e97f9dd259853bc73040efe052c9613ceaf57e
Lauren Biggers <lauren.r.biggers@vumc.org>
2019-07-05 16:26:11 -0500

    DC-417  convention changes.
    Adding and removing spaces as recommended by pylint
    Using dictionary's .get() method to avoid KeyErrors
    Renaming all upper cased local variables to lower cased local variables
    Introducing python command line parsing.  (Function is there but is not being used yet.)
    Removing unused imports and variables.
    Removing dead code.

--- data_steward/deid/src/aou_deid/meta.py
+++ data_steward/deid/src/aou_deid/meta.py
@@ -4,45 +4,48 @@
 
 This is a factory that handles rules generated into a canonical form and sends them to a deid engine for processing
 """
-import pandas as pd
 import sqlite3
+
 from google.cloud import bigquery as bq
+import pandas as pd
 
 
-class Meta :
-    def sqlite(self,**args):
+class Meta(object):
+
+    def sqlite(self, **args):
         """
         Will load a csv/json file with the type information of a table provide the following:
+
         :path   path of the sqlite file
         :table  name of the table
         """
         path = args['path']
         table = args['table']
         conn = sqlite3.connect(path)
-        sql = " ".join(["SELECT * FROM ",table,"LIMIT 10"])
-        df = pd.read_sql_query(sql,conn)
-        return df.columns.tolist()
-    def postgresql(self,**args):
-        return None
-    
-    def bigquery(self,**args):
+        sql = " ".join(["SELECT * FROM ", table, "LIMIT 10"])
+        data_frame = pd.read_sql_query(sql, conn)
+        return data_frame.columns.tolist()
+
+    def postgresql(self, **args):
+        return
+
+    def bigquery(self, **args):
         """
         This function will return the meta data associated with a table in a bigquery dataset (or schema)
         """
-        table_name  = args['table']
-        schema      = args['schema'] if 'schema' in args else args['dataset']
-        private_key = args['path']
+        table_name = args.get('table', '')
+        schema = args.get('schema', args.get('dataset', ''))
+        private_key = args.get('path', '')
         client = bq.Client.from_service_account_json(private_key)
         tables = client.list_tables(bq.dataset(schema))
         tables = [table for table in tables if table.table_id == table_name]
         return tables[0] if tables else None
-    
-    def instance(self,**args):
+
+    def instance(self, **args):
         """
         :path   path of the db (for sqlite), JSON service account file (bigquery)
         :table  name of the table
         :schema optional if the persistance store supports it
         """
-        store = args['store']
-        return getattr(Meta,store)(**args)
-
+        store = args.get('store', '')
+        return getattr(Meta, store)(**args)
--- data_steward/deid/src/aou_deid/parser.py
+++ data_steward/deid/src/aou_deid/parser.py
@@ -2,79 +2,115 @@
     Health Information Privacy Lab
     Brad. Malin, Weiyi Xia, Steve L. Nyemba
 
-    This file is designed to put rules into a canonical form. 
-    The framework distinguishes rules from their applications so as to allow simulation and have visibility into how the rules are applied given a variety of contexts
+    This file is designed to put rules into a canonical form.
+    The framework distinguishes rules from their applications so as to allow
+    simulation and have visibility into how the rules are applied given a variety
+    of contexts
 
 """
+from argparse import ArgumentParser
 import sys
-class Parse:
+
+
+class Parse(object):
     """
-    This utility class implements the various ways in which rules and their applications are put into a canonical form
-    Having rules in a canonical form makes it easy for an engine to apply them in batch
+    This utility class implements the various ways in which rules and their
+    applications are put into a canonical form
+
+    Having rules in a canonical form makes it easy for an engine to apply them
+    in batch
     """
     @staticmethod
-    def init(id,row,cache):
-        if 'rules' in row :
-            _id,_key = row['rules'].replace('@','').split('.')
-        else:
-            _id,_key = None,None
-        label = ".".join([_id,_key]) if _key is not None else id
+    def init(rule_id, row, cache):
+        try:
+            row_rules = row.get('rules', '')
+            _id, _key = row_rules.replace('@', '').split('.')
+        except ValueError:
+            _id, _key = None, None
+
+        label = ".".join([_id, _key]) if _key else rule_id
+
+        p = {'label': label}
         
         if _id and _key  and _id in cache and _key in cache[_id]:
-            p  =  {'label':label,'rules':cache[_id][_key]}
-            
-        else:
-            p =  {'label':label}
-        for key in row :
+            p['rules'] = cache[_id][_key]
+
+        for key in row:
             p[key] = row[key] if key not in p else p[key]
-        
-        return p    
-        
-        
 
-    @staticmethod
-    def shift(row,cache):
-        return Parse.init('shift',row,cache)
-
-    @staticmethod
-    def generalize(row,cache):
-        """
-        parsing generalization and translating the features into a canonical form of stors
-        """
-        p = Parse.init('generalize',row,cache)
-        # if 'into' in row :
-        #     p['fields'] = [row['into']]
-        #     del p['into']
-        # if 'on' in row :
-        
         return p
+
     @staticmethod
-    def suppress(row,cache):
+    def shift(row, cache):
+        return Parse.init('shift', row, cache)
+
+    @staticmethod
+    def generalize(row, cache):
+        """
+        parsing generalization and translating the features into a canonical form of stores
+        """
+        p = Parse.init('generalize', row, cache)
+
+        return p
+
+    @staticmethod
+    def suppress(row, cache):
         """
         setup suppression rules to be applied the the given 'row' i.e entry
         """
-        return Parse.init('suppress',row,cache)
-    @staticmethod
-    def compute(row,cache):
-        return Parse.init('compute',row,cache)
+        return Parse.init('suppress', row, cache)
 
     @staticmethod
-    def sys_args() :
+    def compute(row, cache):
+        return Parse.init('compute', row, cache)
+
+    @staticmethod
+    def sys_args():
         SYS_ARGS = {}
         if len(sys.argv) > 1:
-            
+
             N = len(sys.argv)
-            for i in range(1,N):
+            for i in range(1, N):
                 value = None
                 if sys.argv[i].startswith('--'):
-                    key = sys.argv[i].replace('-','')
+                    key = sys.argv[i].replace('-', '')
                     SYS_ARGS[key] = 1
-                    if i + 1 < N and sys.argv[i+1].startswith('--') is False:
-                        value = sys.argv[i + 1] = sys.argv[i+1].strip()
+
+                    if i + 1 < N and sys.argv[i + 1].startswith('--') is False:
+                        value = sys.argv[i + 1] = sys.argv[i + 1].strip()
+
                     if key and value:
                         SYS_ARGS[key] = value
-                
-                i += 2        
+
+                i += 2
         return SYS_ARGS
-#
-# This part will parse system arguments
+
+
+def parse_args():
+    parser = ArgumentParser(description='Parse deid command line arguments')
+    parser.add_argument('--rules',
+                        action='store', dest='rules',
+                        help='Filepath to the JSON file containing rules',
+                        required=True)
+    parser.add_argument('--idataset',
+                        action='store', dest='idataset',
+                        help=('Name of the input dataset (an output dataset '
+                              'with suffix _deid will be generated)'),
+                        required=True)
+    parser.add_argument('--private_key', dest='private_key', action='store',
+                        required=True,
+                        help='Service account file location')
+    parser.add_argument('--table', dest='table', action='store', required=True,
+                        help='Path that specifies how rules are applied on a table')
+    parser.add_argument('--action', dest='action', action='store', required=True,
+                        choices=['submit', 'simulate', 'debug'],
+                        help=('simulate: generate simulation without creating an '
+                              'output table\nsubmit: create an output table\n'
+                              'debug: print output without simulation or submit '
+                              '(runs alone)')
+                        )
+    parser.add_argument('--cluster', dest='cluster', action='store_true',
+                        help='Enable clustering on person_id')
+    parser.add_argument('--log', dest='log', action='store',
+                        help='Filepath for the log file')
+    return parser.parse_args()
